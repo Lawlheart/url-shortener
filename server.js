34d4f41f8be60b3240c7fef54dbf421b6485873d
LawlietBlack <kenneth.black@live.com>
2016-02-18 15:07:14 -0800

    set dotenv to only work in production

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var passport = require('passport');
 var session = require('express-session');
 
 var app = express();
-require('dotenv').load();
+
+if(process.env.COMPUTERNAME === 'FORGE') {
+  require('dotenv').load();
+}
 require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
